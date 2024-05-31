8c48bd2d24730b49569cfee17a92a8d4cae2df85
Mohan <telecomst7@wichita.edu>
2024-05-31 11:19:33 -0500

    Initial test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  return(
+    <Header/>
+  )
 }
 
-export default App;
+function Header(){
+
+let [count, setCount] = useState(0);
+
+function positive(){
+    setCount(count+1);
+}
+function negative(){
+  setCount(count-1);
+}
+
+  return(
+    <div>
+<button className="btn" onClick={positive}>Increase</button>
+<p>{count}</p>
+<button className="btn" onClick={negative}>Decrease</button>
+    </div>
+  )
+}
